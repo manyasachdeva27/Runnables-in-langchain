416fc97fd6ae6310327f7ebe19e8018c337bf4f1
manyasachdeva27 <sachdevamanya92@gmail.com>
2026-03-29 16:01:08 +0530

    Add folder

---
+++ llmchain.py
@@ -0,0 +1,24 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+#initialize the LLM
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key="AIzaSyAMNx92HnX32TtDp_9yCm76x3L-vY617P4")
+
+#define the prompt template
+prompt = PromptTemplate(
+    input_variables=["topic"],
+    template="Suggest a catchy blog about {topic}?")
+
+#create a LLMChain
+chain=LLMChain(llm=llm,prompt=prompt)
+
+#define the topic
+topic=input('enter the topic')
+output=chain.run(topic)
+
+#print the output
+print("generated blog title:",output)
+
+
+5
---
+++ simple_llm_app.py
@@ -0,0 +1,24 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+
+#initialize the LLM
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key="AIzaSyAMNx92HnX32TtDp_9yCm76x3L-vY617P4")
+
+#define the prompt template
+prompt = PromptTemplate(
+    input_variables=["topic"],
+    template="Suggest a catchy blog about {topic}?")
+
+#define the topic
+topic=input('enter the topic')
+
+#format the prompt manually using PromptTemplate
+formatted_prompt=prompt.format(topic=topic)
+
+#call the llm manually
+blog_title=llm.predict(formatted_prompt)
+
+#print the output
+print("generated blog title:",blog_title)
+
+
